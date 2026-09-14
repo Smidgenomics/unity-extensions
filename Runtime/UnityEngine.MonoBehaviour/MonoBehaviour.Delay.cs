ecfb04d24600cc336199f6b85a9c4d81f5411e62
Unnar <unnarvc@speedymail.org>
2026-09-14 13:58:42 +0000

    Refactor delay code

--- Runtime/UnityEngine.MonoBehaviour/MonoBehaviour.Delay.cs
+++ Runtime/UnityEngine.MonoBehaviour/MonoBehaviour.Delay.cs
@@ -9,34 +9,34 @@ namespace Smidgenomics.Unity.Extensions
 	public static partial class MonoBehaviour_
 	{
 		/// <summary>
-		/// Executes callback after given delay
+		/// Executes callback after delay
 		/// </summary>
-		/// <param name="b">Behavior</param>
-		/// <param name="delay">Delay (secs)</param>
-		/// <param name="afterDelay">Callback</param>
-		/// <returns>Coroutine</returns>
 		public static Coroutine Delay(this MonoBehaviour b, in float delay, Action afterDelay)
 		{
 			return b.Delay(new WaitForSeconds(delay), afterDelay);
 		}
 
 		/// <summary>
-		/// Executes callback after given 
+		/// Executes callback after delay
 		/// </summary>
-		/// <param name="b">Behavior</param>
-		/// <param name="delay">Delay (yield)</param>
-		/// <param name="afterDelay">Callback</param>
-		/// <returns>Coroutine</returns>
-		/// <exception cref="ArgumentException"></exception>
 		public static Coroutine Delay(this MonoBehaviour c, WaitForSeconds delay, Action afterDelay)
 		{
-			return c.StartCoroutine(DelayRoutine(delay, afterDelay));
+			return c.StartCoroutine(YieldRoutine(delay, afterDelay));
 		}
 
-		// delay helper
-		private static IEnumerator DelayRoutine(WaitForSeconds w, Action fn)
+		/// <summary>
+		/// Executes callback at the end of frame
+		/// </summary>
+		public static Coroutine DelayTilEndOfFrame(this MonoBehaviour c, Action afterDelay)
 		{
-			yield return w;
+			return c.StartCoroutine(YieldRoutine(new WaitForEndOfFrame(), afterDelay));
+		}
+
+		
+		// delay helper
+		private static IEnumerator YieldRoutine(YieldInstruction yi, Action fn)
+		{
+			yield return yi;
 			fn.Invoke();
 		}
 	}
